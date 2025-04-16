08c2ed543b4b9bbddef1461aaeb78141a42626c0
Yash-Swaminathan <157097653+Yash-Swaminathan@users.noreply.github.com>
2025-04-16 19:11:46 +0000

    Deploying to gh-pages from @ Yash-Swaminathan/Yash-Swaminathan.github.io@ae5e283176cccfa8fdd8efc3d3293424a06ab77d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "A concise overview of my education, work experience, skills, and projects—viewable below on this page. You can also view the full PDF resume inline.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
